3a0d84927e3b05c07600c74a79f72ba64ea0f8da
Teleportal <joshthegreenberg@gmail.com>
2018-01-09 18:48:16 -0600

    learned about superclasses and subclasses and the concept of inheritance

--- employee.rb
+++ employee.rb
@@ -12,32 +12,35 @@
 # puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes $#{employee_2[:salary]} a year."
 
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary =  input_salary
-    @active = input_active
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary =  input_options[:salary]
+    @active = input_options[:active]
   end
 
-  def first_name
-    @first_name
-  end
+  # def first_name
+  #   @first_name
+  # end
 
-  def last_name
-    @last_name
-  end
+  # def last_name
+  #   @last_name
+  # end
 
-  def salary
-    @salary
-  end
+  # def salary
+  #   @salary
+  # end
 
-  def active
-    @active
-  end
+  # def active
+  #   @active
+  # end
 
-  def active=(new_active)
-    @active = new_active
-  end
+  # def active=(new_active)
+  #   @active = new_active
+  # end
 
   def print_info
     puts "#{@first_name} #{@last_name} makes $#{@salary} a year"
@@ -50,7 +53,52 @@ class Employee
 
 end
 
-employee_1 = Employee.new("Bruce", "Wayne", 70000, true)
+employee_1 = Employee.new(
+                          first_name: "Bruce", 
+                          last_name: "Wayne", 
+                          salary: 70000, 
+                          active: true
+                          )
+
+
 employee_1.print_info
 employee_1.give_annual_raise
 employee_1.print_info
+
+class Manager < Employee
+  # attr_reader :first_name, :last_name, :salary, :active
+  # attr_writer :active
+
+  # def initialize(input_options)
+  #   @first_name = input_options[:first_name]
+  #   @last_name = input_options[:last_name]
+  #   @salary =  input_options[:salary]
+  #   @active = input_options[:active]
+  # end
+
+  # def print_info
+  #   puts "#{@first_name} #{@last_name} makes $#{@salary} a year"
+    
+  # end
+
+  # def give_annual_raise
+  #   @salary = @salary * 1.05
+  # end
+
+  def send_report
+    puts "Sending Email..."
+    #code that sends email
+    puts "Email sent."
+  end
+
+end
+
+manager = Manager.new(
+                      first_name: "Clark",
+                      last_name: "Kent",
+                      salary: 100000,
+                      active: true
+                      )
+
+manager.print_info
+manager.send_report
